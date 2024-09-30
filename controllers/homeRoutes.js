30640458f266c3683606b31390003ae865bc6035
Meadoughnut <127433011+Meadoughnut@users.noreply.github.com>
2024-09-30 19:38:37 -0400

    final update

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,76 +3,83 @@ const router = require('express').Router();
 const { Post, User } = require('../models');
 const withAuth = require('../utils/auth');
 
-
 router.get('/', async (req, res) => {
     try {
-      
-      const postData = await Post.findAll({
-        include: [
-          {
-            model: User,
-            attributes: ['name'],
-          },
-        ],
-      });
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
 
-      const posts = postData.map((post) => post.get({ plain: true }));
+        const posts = postData.map((post) => post.get({ plain: true }));
 
-      res.render('homepage', { 
-        posts, 
-        logged_in: req.session.logged_in 
-      });
+        res.render('homepage', {
+            posts,
+            logged_in: req.session.logged_in,
+        });
     } catch (err) {
-      res.status(500).json(err);
+        res.status(500).json(err);
     }
-  });
+});
 
-  router.get('/post/:id', async (req, res) => {
+router.get('/post/:id', async (req, res) => {
     try {
-      const postData = await Post.findByPk(req.params.id, {
-        include: [
-          {
-            model: User,
-            attributes: ['username'],
-          },
-        ],
-      });
+        const postData = await Post.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+            ],
+        });
 
-      const post = postData.get({ plain: true });
+        const post = postData.get({ plain: true });
 
-      res.render('post', {
-        ...post,
-        logged_in: req.session.logged_in
-      });
+        res.render('post', {
+            ...post,
+            logged_in: req.session.logged_in,
+        });
     } catch (err) {
-      res.status(500).json(err);
+        res.status(500).json(err);
     }
-  }),
+});
 
-  router.get('/profile', withAuth, async (req, res) => {
+router.get('/profile', withAuth, async (req, res) => {
     try {
-      const userData = await User.findByPk(req.session.user_id, {
-        attributes: { exclude: ['password'] },
-        include: [{ model: Post }],
-      });
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: Post }],
+        });
 
-      const user = userData.get({ plain: true });
+        const user = userData.get({ plain: true });
 
-      res.render('profile', {
-        ...user,
-        logged_in: true
-      });
+        res.render('profile', {
+            ...user,
+            logged_in: true,
+        });
     } catch (err) {
-      res.status(500).json(err);
+        res.status(500).json(err);
     }
-  }),
+});
 
-  router.get('/login', (req, res) => {
+router.get('/login', (req, res) => {
     if (req.session.logged_in) {
-      res.redirect('/profile');
-      return;
+        res.redirect('/profile');
+        return;
     }
     res.render('login');
-  });
+});
+
+// New signup route
+router.get('/signup', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/profile');
+        return;
+    }
+    res.render('signup');
+});
 
 module.exports = router;
