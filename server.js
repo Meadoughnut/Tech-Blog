adeada63beab25880e9f3bfe6f9d24e54c94a5d7
Meadoughnut <127433011+Meadoughnut@users.noreply.github.com>
2024-09-30 18:56:44 -0400

    final update

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
@@ -10,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create({});
+const hbs = exphbs.create();
 
 const sess = {
   secret: 'Super secret secret',
@@ -30,6 +31,7 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
